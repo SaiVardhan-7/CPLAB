2d0b390c501cc976f8f21b6d50b4a447581ae9c3
SaiVardhan-7 <kattasaivardhan838@gmail.com>
2024-09-18 21:43:58 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+  #include<stdio.h>
+int main()
+{
+//sum of digits of a number
+int s=0,n;
+   printf("Enter the value of n: ");
+   scanf("%d",&n);
+ while(n>=1){
+     s=s+n%10;
+     n=n/10;
+ }
+  printf("%d",s);
+    return 0;
+}
