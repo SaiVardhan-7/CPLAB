a90328aee7906a8973481e653f7512555657d31a
SaiVardhan-7 <kattasaivardhan838@gmail.com>
2024-09-19 14:37:28 +0530

    distance.c

---
+++ Week1/distance.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+     float v,d,t;
+     
+     printf("Enter the value of velocity and time");
+     
+     scanf("%f%f",&v,&t);
+     
+     d=v*t;
+     
+     printf("Distance travelled by an object is  %f",d);
+
+     
+}
+     
+     
